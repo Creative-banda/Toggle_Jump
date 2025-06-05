12e1ead925c4ea5645c841db1e8c8a408328a586
Creative-banda <4kpuspa@gmail.com>
2025-06-05 12:22:53 +0530

    Changed the title of the game

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ SCREEN_HEIGHT = 600
 #create game window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-pygame.display.set_caption('Jumpy')
+pygame.display.set_caption('Space Jumper')
 
 #set frame rate
 clock = pygame.time.Clock()
